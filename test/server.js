2eb756b79d5ea5547c5781466d58d4b1c6b24cdc
Phil Gambling <pgambling@gmail.com>
2020-12-01 14:23:20 -0600

    fixed test api

--- test/server.js
+++ test/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/test", (_req, res) => res.send('Success!'));
+app.get("/", (_req, res) => res.send('Success!'));
 
 console.log('Server listening on port 3000')
 app.listen(3000);

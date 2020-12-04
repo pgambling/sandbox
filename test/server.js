e6e45e784c3a8c734fa219d77da6ef3ead10f09a
Phil Gambling <phil.gambling@experoinc.com>
2020-12-04 11:20:15 -0600

    data collection webhook

--- test/server.js
+++ test/server.js
@@ -1,8 +1,14 @@
+const fs = require('fs');
 const express = require("express");
 
 const app = express();
+app.use(express.json())
 
 app.get("/", (_req, res) => res.send('Success!'));
+app.post("/", (req, res) => {
+  fs.writeFileSync('webhook-data.json', JSON.stringify(req.body, null, 2))
+  res.end();
+});
 
 console.log('Server listening on port 3000')
 app.listen(3000);

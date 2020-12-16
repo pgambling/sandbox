f29afd695764e2f0954a8e2bb846ece1c63dda3a
Phil Gambling <phil.gambling@experoinc.com>
2020-12-16 13:29:19 -0600

    webhook for wifi state tracking test

--- test/server.js
+++ test/server.js
@@ -9,6 +9,13 @@ app.post("/", (req, res) => {
   fs.writeFileSync('webhook-data.json', JSON.stringify(req.body, null, 2))
   res.end();
 });
+app.post("/wifi-state", (req, res) => {
+  const fileName = 'wifi-state.json';
+  const data = JSON.parse(fs.readFileSync(fileName));
+  data.push(req.body);
+  fs.writeFileSync(fileName, JSON.stringify(data, null, 2))
+  res.end();
+})
 
 console.log('Server listening on port 3000')
 app.listen(3000);
